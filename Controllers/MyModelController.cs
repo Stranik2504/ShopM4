5ca48dc14c5e75d8cdaa066c74b254f78a192dcd
Stranik2504 <58374405+Stranik2504@users.noreply.github.com>
2022-12-03 18:30:53 +0300

    Add delete, edit and create functions for MyModel

--- Controllers/MyModelController.cs
+++ Controllers/MyModelController.cs
@@ -16,5 +16,65 @@ namespace AdventureLabNew.Controllers
 
             return View(categories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(MyModel myModel)
+        {
+            if (!ModelState.IsValid)
+                return View(myModel);
+
+            _db.MyModels.Add(myModel);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == default || id == 0)
+                return NotFound();
+
+            var myModel = _db.MyModels.Find(id);
+
+            if (myModel == default)
+                return NotFound();
+
+            return View(myModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(MyModel myModel)
+        {
+            if (!ModelState.IsValid)
+                return View(myModel);
+
+            _db.MyModels.Update(myModel);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int? id)
+        {
+            var myModel = _db.MyModels.Find(id);
+
+            if (myModel == default)
+                return NotFound();
+
+            _db.MyModels.Remove(myModel);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
